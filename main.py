b887b781d13456a8ec000ea51754ceb33c0a9249
Rashmi-PP <rashminambiarpp22@gmail.com>
2022-11-06 22:43:37 -0500

    updated files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # Dynamic URL 
 from flask import Flask, redirect, url_for, render_template, request
 
-app = Flask(__name__)               
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/home')
 def home():
     return render_template('index.html')
